ce86c062528ca05503df5f5642bdeace77c0094e
Ravi Gerardo <ravishgerardo@gmail.com>
2016-12-29 15:11:05 -0600

    Add files via upload

---
+++ cy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo "Usuario: "
+read USR
+#USR="nombre del usuario"
+echo "Directorio: "
+read DIR
+while [ ! -d /media/$USR/* ]; do
+   Cero=0
+done
+cd /media/$USR
+test -d /media/$USR/* && mkdir /home/$USR/$DIR ; sudo cp -r /media/$USR/* /home/$USR/$DIR
+sudo chmod 777 -R /home/$USR/$DIR/*
+cd -
+sudo chmod 777 -R /home/$USR/$DIR
