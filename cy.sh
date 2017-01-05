692811964feff828cb44e580ffa6dc5c527d668a
Ravi Gerardo <ravishgerardo@gmail.com>
2017-01-05 16:37:26 -0600

    Update cy.sh

--- cy.sh
+++ cy.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-echo "Usuario: "
-read USR
-#USR="nombre del usuario"
+#echo "Usuario: "
+#read USR
+USR="ravi"
 echo "Directorio: "
 read DIR
 while [ ! -d /media/$USR/* ]; do
    Cero=0
 done
-cd /media/$USR
-test -d /media/$USR/* && mkdir /home/$USR/$DIR ; sudo cp -r /media/$USR/* /home/$USR/$DIR
-sudo chmod 777 -R /home/$USR/$DIR/*
+mkdir /home/$USR/$DIR
+sudo cp -r /media/$USR/* /home/$USR/$DIR
+sudo chmod +777 -R /home/$USR/$DIR
